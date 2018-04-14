bc7dee6dcffad846dc4c7a5bc586a2b0e5f14480
Akshay Bhagwat <akshaybhagwat76@gmail.com>
2018-04-14 14:15:58 +0530

    add img to pdf converters

--- PDFtoIMG/Controllers/HomeController.cs
+++ PDFtoIMG/Controllers/HomeController.cs
@@ -94,18 +94,29 @@ namespace PDFtoIMG.Controllers
                         }
                     }
                 }
-                else
+               else
                 {
-                    using (var converter= new Aspose.Pdf.Facades.PdfConverter())
-                    {
-                        converter.BindPdf(FolderPath);
-                        converter.DoConvert();
-                        var settings = new Aspose.Pdf.Devices.JpegDevice()
-                        {
-                            Compression = Aspose.Pdf.Devices.CompressionType.RLE,
-                        };
-                    }
-                    doc.Save(Server.MapPath("~/PdfFolder/") + "1" + ".pdf");
+                    //Document imgDocument = new Document(FolderPath);
+                    Aspose.Pdf.Document pdfDocument = new Aspose.Pdf.Document();
+                    pdfDocument.Pages.Add();
+                    int lowerLeftX = 100;
+                    int lowerLeftY = 100;
+
+                    int upperRightX = 200;
+                    int upperRightY = 200;
+                    Page page = pdfDocument.Pages[1];
+                    FileStream imageStream = new FileStream(FolderPath, FileMode.Open);
+                    page.Resources.Images.Add(imageStream);
+                    page.Contents.Add(new Operator.GSave());
+                    Aspose.Pdf.Rectangle rectangle = new Aspose.Pdf.Rectangle(lowerLeftX, lowerLeftY, upperRightX, upperRightY);
+                    Aspose.Pdf.DOM.Matrix matrix = new Aspose.Pdf.DOM.Matrix(new double[] { rectangle.URX - rectangle.LLX, 0, 0, rectangle.URY - rectangle.LLY, rectangle.LLX, rectangle.LLY });
+                    page.Contents.Add(new Operator.ConcatenateMatrix(matrix));
+                    XImage ximage = page.Resources.Images[page.Resources.Images.Count];
+                    page.Contents.Add(new Operator.Do(ximage.Name));
+                    page.Contents.Add(new Operator.GRestore());
+                    //var fileName1 = DateTime.Now.ToString("dd_MM_yyyy_hh_mm_ss") + ext;
+                    //uploadpath = string.Format("{0}\\{1}", path.ToString(), fileName1);
+                    pdfDocument.Save(Server.MapPath("~/PdfFolder/") + "Image -In-PDF.pdf");
                 }
 
             }
